97e63a60ca697cacd373d3b57f4a1a81d166db33
Abir Sarkar <abirsarkar1999@gmail.com>
2024-07-07 12:04:04 +0530

    "mongoDB error fixed"

--- src/db/index.js
+++ src/db/index.js
@@ -2,9 +2,12 @@ import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('DATABASE_URL environment variable is not set');
+  }
+
   try {
-    print(process.env.MONGO_URI);
-    const connectionInstance = await mongoose.connect(process.env.MONGO_URI, {
+    const connectionInstance = await mongoose.connect(process.env.DATABASE_URL, {
       dbName: DB_NAME,
     });
     console.log(`MongoDB connected: ${connectionInstance.connection.host}`);
--- src/index.js
+++ src/index.js
@@ -1,21 +1,10 @@
 import dotenv from "dotenv";
 import express from "express";
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 import connectDB from "./db/index.js";
 
 dotenv.config({
-    path: "/.env"
+    path: "./env",
 });
-
-print(process.env.MONGO_URI);
-connectDB();
 
 const app = express();
-
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
-
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
-});
